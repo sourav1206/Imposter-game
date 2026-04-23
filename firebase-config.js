ad5623df3e403e84bc1e12592f87dce1a6e3b149
Sourav Choudhary <65532842+sourav1206@users.noreply.github.com>
2026-04-23 22:14:04 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+// Firebase configuration for Imposter Game
+// Note: Firebase API keys are public identifiers, not secrets.
+// See: https://firebase.google.com/docs/projects/api-keys
+// Security is handled via Firebase Realtime Database Rules.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDbcsC_JRak32yW9gsOXQQldeC-G7p7jIU",
+  authDomain: "imposter-game-5e141.firebaseapp.com",
+  databaseURL: "https://imposter-game-5e141-default-rtdb.firebaseio.com",
+  projectId: "imposter-game-5e141",
+  storageBucket: "imposter-game-5e141.firebasestorage.app",
+  messagingSenderId: "1092575215270",
+  appId: "1:1092575215270:web:73b9d24c39476c0b37be67"
+};
